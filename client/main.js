44ed06e4cc6e1a8df23a03ec29b7d28f035f7a78
La7rodectus <maxmega27@gmail.com>
2021-02-04 13:45:53 +0200

    add socket connection to server

--- client/main.js
+++ client/main.js
@@ -4,13 +4,29 @@
 //client.send(JSON.stringify({mType: 'usersOnline', data: n}));
 //connection.send(JSON.stringify({mType: 'uID', data: id}));
 
-const testName = 'vasya';
+let socket = undefined;
 
-document.addEventListener('DOMContentLoaded', () => {
-  const socket = new WebSocket(`ws://localhost:5000?userName=${testName}`);
-  socket.addEventListener('message', event => {
-    const messageFromServer = event.data;
-    const message = JSON.parse(messageFromServer);
-    console.log(message);
-  });
+//socket msg handlers
+const connectToSIgame = (name) => {
+  socket = new WebSocket(`ws://localhost:5000?userName=${name}`);
+  socket.onopen = () => {
+    socket.onclose = () => {
+      //disconnect();
+      console.log('closed');
+    };
+    socket.onmessage = msg => console.log(JSON.parse(msg.data));
+  };
+};
+
+
+const handleClick = evt => ({
+  'AAAAAAAAAAAAAAAAAAA': connectToSIgame('vasya'),
+})[evt.target.id];
+
+document.addEventListener('click', evt => {
+  if (handleClick(evt) === undefined) {
+    return;
+  } else {
+    handleClick(evt)();
+  }
 });
