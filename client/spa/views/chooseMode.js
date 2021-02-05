00cf74c6ebb99deba671c254ac890046e0309c05
gazinaft <gazinaft@gmail.com>
2021-02-05 16:40:32 +0200

    Add chooseMode view

---
+++ client/spa/views/chooseMode.js
@@ -0,0 +1,9 @@
+const view = () => {
+  return `<div class="container" style="max-width: 500px; margin-top: 15vh">
+    <button id="new-game-btn" type="button" class="btn btn-primary btn-50">Create new game</button>
+    <button id="join-btn" type="button" class="btn btn-primary btn-50">Join lobby</button>
+  </div>
+  `;
+};
+
+export default view;
