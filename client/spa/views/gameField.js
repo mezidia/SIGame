fe0df6c5ec2cc7b94a64edd9512db9712bb2d027
gazinaft <gazinaft@gmail.com>
2021-02-10 13:08:17 +0200

    gameField additions

---
+++ client/spa/views/gameField.js
@@ -0,0 +1,41 @@
+export default class GameField {
+
+  // returns a game table used in classic mode
+  static drawTable() {
+    const drawCells = () => {
+      const res = [];
+      for(let i = 1; i<=5; ++i) {
+        for(let j = 1; j<=5; ++j) {
+          res.push(`<div class="centred-text" id="cell-${i}-${j}"></div>`)
+        }
+      }
+      return res.join('\n');
+    }
+    return `<div class="col-3" style="background-color: #c7280e; padding: 0 0 0 0;">
+      <div class="vertical-grid-5">
+        <div class="centred-text" id="theme-1"></div>
+        <div class="centred-text" id="theme-2"></div>
+        <div class="centred-text" id="theme-3"></div>
+        <div class="centred-text" id="theme-4"></div>
+        <div class="centred-text" id="theme-5"></div>
+      </div>
+    </div>
+    <div class="col-9" style="background-color: #f36a54; padding: 0 0 0 0;">
+      <div class="table-25">
+        ${drawCells()}
+      </div>
+    </div>
+    `;
+  }
+  // draws a question and reads it.
+  // When the animation is over you can listen to it via
+  // animationend listener. There's an example of it in main.js 141 line
+  static drawQuestion(str) {
+    // noinspection CssInvalidPropertyValue
+    return `<span id="question-text">${[...str].map((letter, index) =>
+      `<span ${(index === str.length - 1) ? 'id="last-letter"': ''}
+          class="question-letter" style="animation-duration: ${index * 0.16}s">${letter}</span>`).join('')}
+    </span>`
+  }
+
+}
--- client/spa/views/simpleLobby.js
+++ client/spa/views/simpleLobby.js
@@ -1,14 +1,19 @@
+import GameField from './gameField.js';
+
 const view = () => {
-  return `<div class="row" style="margin: 0px 0px 0px 0px; height: 100%">
-    <div class="col-md-9 game-container" style="padding: 0px 0px 0px 0px">
-      <div id="game-display" style="background-color: #2aaacf; min-height: 200px"></div>
+  return `<div class="row" style="margin: 0 0 0 0; height: 100%">
+    <div class="col-md-9 game-container" style="padding: 0 0 0 0">
+      <div id="game-display" class="row" style="min-height: 200px; margin: 0 0 0 0">
+        <!-- Gamefield class methods should be injected here -->
+      </div>
+      
       <div id="players-icons" style="background-color: #3c9a5f"></div>
       <div id="reply" style="background-color: #7c7c7c">
         <input type="text" style="display: block; width: calc(100% - 100px); height: 100%; float: left">
         <button class="game-button" style="width: 100px; height: 100%"></button>
       </div>
     </div>
-    <div class="col-md-3 chat-container" style="padding: 0px 0px 0px 0px">
+    <div class="col-md-3 chat-container" style="padding: 0 0 0 0">
       <div id="chat" style="background-color: #6f42c1; min-height: 70px; overflow: auto"></div>
       <div id="chat-input" style="background-color: #8c8c8c;">
         <input type="text" style="width: 100%; height: 50px;">
