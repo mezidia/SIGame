d7fca4dfe696a890a5652c1099ad531156eb0306
La7rodectus <maxmega27@gmail.com>
2021-02-17 12:22:42 +0200

    fix langcode localization bug, add q click

--- client/gameLogic/game_class.js
+++ client/gameLogic/game_class.js
@@ -1,26 +1,58 @@
 'use strict';
 
 import GameField from "../spa/views/gameField.js";
+import User from "./user_class.js";
 
 export default class Game {
+  _setListeners() {
+    document.addEventListener('click', this.clickHandler);
+  }
 
-  constructor(bundle, settings, socket) {
+  _removeListeners() {
+
+  }
+
+  constructor(bundle, settings) {
     this._id = undefined;
-    this._socket = socket;
-    this.master = settings.socket;
+    this._socket = new User().socket;
+    this.master = new User().name;
+    this.roomName = settings.roomName;
+    this.maxPpl = settings.ppl;
+    this.password = settings.password;
+    this.gameMode = settings.gameMode;
     this.bundle = bundle;
     this.players = {};
     this.gameField = new GameField();
+    this._setListeners();
     console.log('new Game');
   }
 
+  clickHandler = (e) => {
+    const target = e.target;
+    if (!target.classList.contains('q-cell')) return;
+    const splitedID = target.id.split('-');
+    const i = splitedID[1] - 1;
+    const j = splitedID[2] - 1;
+    const q = this.bundle.decks[i].questions[j];
+    console.log(q);
+    this.gameField.drawQuestion(q.string);
+  }
+
   init() {
-    this.gameField.drawTable();
+    this.gameField.drawTable(this.bundle.round_1);
   }
 
-  broadcast() {
+  setupQuestiones() {
 
   }
+
+  broadcast(event) {
+    this._socket.send(JSON.stringify({ mType: 'broadcastInRoom', data: {
+      event: event,
+      roomID: this._id,
+    }})); 
+  }
+
   addPlayer(name) {
     this.players.name = name;
   }
@@ -35,6 +67,10 @@ export default class Game {
 
   gameLoop() {
 
+  }
+
+  setMaster(master) {
+    this.master = master;
   }
 
   setID(id) {
--- client/spa/views/gameField.js
+++ client/spa/views/gameField.js
@@ -8,29 +8,32 @@ export default class GameField {
     return GameField._instance;
   }
   // returns a game table used in classic mode
-  drawTable() {
+  drawTable(deck) {
     const gameDisplay = document.getElementById('game-display');
-    const drawCells = () => {
+    const drawCells = (deck) => {
+      console.log(deck[0]);
       const res = [];
-      for(let i = 1; i<=5; ++i) {
-        for(let j = 1; j<=5; ++j) {
-          res.push(`<div class="centred-text" id="cell-${i}-${j}"></div>`)
+      for(let i = 1; i <= 5; ++i) {
+        for(let j = 1; j <= 5; ++j) {
+          const questionCost = deck[i - 1].questions[j - 1].cost;
+          if (!questionCost) questionCost = '';
+          res.push(`<div class="centred-text q-cell" id="cell-${i}-${j}">${questionCost}</div>`)
         }
       }
       return res.join('\n');
     }
     gameDisplay.innerHTML = `<div class="col-3" style="background-color: #c7280e; padding: 0 0 0 0;">
       <div class="vertical-grid-5">
-        <div class="centred-text" id="theme-1"></div>
-        <div class="centred-text" id="theme-2"></div>
-        <div class="centred-text" id="theme-3"></div>
-        <div class="centred-text" id="theme-4"></div>
-        <div class="centred-text" id="theme-5"></div>
+        <div class="centred-text" id="theme-1">${deck[0].subject}</div>
+        <div class="centred-text" id="theme-2">${deck[1].subject}</div>
+        <div class="centred-text" id="theme-3">${deck[2].subject}</div>
+        <div class="centred-text" id="theme-4">${deck[3].subject}</div>
+        <div class="centred-text" id="theme-5">${deck[4].subject}</div>
       </div>
     </div>
     <div class="col-9" style="background-color: #f36a54; padding: 0 0 0 0;">
       <div class="table-25">
-        ${drawCells()}
+        ${drawCells(deck)}
       </div>
     </div>
     `;
