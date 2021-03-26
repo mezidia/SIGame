9419daaa8bef60f6aa0892e138c9f502f4e7eecd
gazinaft <gazinaft@gmail.com>
2021-03-26 19:30:20 +0200

    img tag fix

--- client/spa/views/gameField.js
+++ client/spa/views/gameField.js
@@ -211,7 +211,7 @@ export default class GameField {
 
   waitForPlayersJpgShow() {
     const gameDisplay = document.getElementById('game-display');
-    gameDisplay.innerHTML = `<img style="display: block; width: 50%; height: auto; margin-left: auto; margin-right: auto;" src="lobbySearchImage.jpg"></img>`;
+    gameDisplay.innerHTML = `<img style="display: block; width: 50%; height: auto; margin-left: auto; margin-right: auto;" src="lobbySearchImage.jpg" alt="Waiting for start">`;
   }
 
 }
