6a3f466cc2e54a5f300bbb9b0823ba79e550d488
gazinaft <gazinaft@gmail.com>
2021-04-05 16:39:59 +0300

    swap places buttons

--- client/spa/views/mainPage.js
+++ client/spa/views/mainPage.js
@@ -5,8 +5,8 @@ const view = () => {
     <h1 style="text-align: center" data-localize="title">SI Game</h1>
     <form class="column-content" id="name">
       <input id="name-input" type="text" placeholder="Enter your name" pattern="[A-Za-zА-яҐґЇїІіЄєäöüÄÖÜß0-9']+" title="May contain letters and/or numbers only" maxlength=34 style="min-height: 50px" data-localize="name" required>
-      <button id="openEditor-btn" type="submit" class="btn btn-primary btn-50" data-localize="bundle">Create bundle</button>
       <button id="play-btn" type="submit" class="btn btn-primary btn-50" data-localize="play">Start game</button>
+      <button id="openEditor-btn" type="submit" class="btn btn-primary btn-50" data-localize="bundle">Create bundle</button>
     </form>
   </div>
   `;
