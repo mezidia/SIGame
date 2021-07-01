a2a86ba7e60cb6160e24c92600b216aea50cac56
Kostya <gazinaft@gmail.com>
2021-07-01 15:02:09 +0300

    loader color

--- client/utils/loader.js
+++ client/utils/loader.js
@@ -3,7 +3,7 @@
 export const loader = () => {
     document.getElementById('main').innerHTML = `
     <div class="text-center" style="margin-top: 15%;">
-      <div class="spinner-border text-primary" role="status" style="width: 5rem; height: 5rem;">
+      <div class="spinner-border " role="status" style="width: 5rem; height: 5rem;">
         <span class="sr-only">Loading...</span>
       </div>
     </div>
