a4a59c4b5c9760b2b23ba4a0d54804c818dfcd3e
La7rodectus <maxmega27@gmail.com>
2021-07-02 17:07:06 +0300

    add img asn audio to bundle editor

--- client/gameLogic/question_class.js
+++ client/gameLogic/question_class.js
@@ -7,6 +7,14 @@ export default class Question {
     this.trueAns = data.trueAns;
     this.falseAns = data.falseAns;
     this.cost = data.cost;
+    this.audio = data.audio;
+    this.img = data.img;
+  }
+
+  getVisualizationType() {
+    if (this.audio) return 'audio';
+    else if (this.img) return 'img';
+    else return 'txt';
   }
 
 }
