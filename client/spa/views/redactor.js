5228c53db26b8c771a3c890d8f095cda2897b1bf
gazinaft <gazinaft@gmail.com>
2021-08-07 17:51:14 +0300

    Update redactor.js

--- client/spa/views/redactor.js
+++ client/spa/views/redactor.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const view = () => {
-  const questionList = (roundNumber, themeNumber) => {
-    return [1, 2, 3, 4, 5].map(i => `
+  const questionList = (roundNumber, themeNumber, questionCount = 5) => {
+    return Array(questionCount).fill(1).map((x, i) => i + 1).map(i => `
       <h5><span data-localize="question">Question</span> ${i}: ${i*roundNumber}00 <span data-localize="points">points</span></h5>
       <p data-localize="sep-answers">Separate true and wrong answers via comas</p>
       <input type="text" placeholder="Question" maxlength=200 id="question-${roundNumber}-${themeNumber}-${i}" data-localize="input-question" required>
       <input type="text" placeholder="Answers" maxlength=300 id="answer-${roundNumber}-${themeNumber}-${i}" data-localize="answers" required>
       <input type="text" placeholder="Wrong answers(optional)" maxlength=300 id="wrong-answer-${roundNumber}-${themeNumber}-${i}" data-localize="wrong-answers-input">
+      <input type="number" placeholder="Question price(optional)" id="question-cost-${roundNumber}-${themeNumber}-${i}" data-localize="question-price">      
       <h5 data-localize="optional">Optional</h5>
       <div id="audio_cont-${roundNumber}-${themeNumber}-${i}">
         <label for="audio" data-localize="input-audio">Select audio for a question</label>
@@ -20,8 +21,8 @@ const view = () => {
       `).join('\n');
   }
 
-  const finalQuestionList = () => {
-    return [1, 2, 3, 4, 5, 6, 7].map(i => `
+  const finalQuestionList = (questionCount = 7) => {
+    return Array(questionCount).fill(1).map((x, i) => i + 1).map(i => `
       <h5 data-localize="question">Question ${i}</h5>
       <input type="text" placeholder="Category" id="final-theme-${i}" data-localize="input-category" required>  
       <br>
@@ -30,6 +31,7 @@ const view = () => {
       <input type="text" placeholder="Answers" maxlength=300 id="answer-4-1-${i}" data-localize="answers" required>
       <input type="text" placeholder="Wrong answers(optional)" maxlength=300 id="wrong-answer-4-1-${i}" data-localize="wrong-answers-input">
       <input type="text" placeholder="Question type" id="question-type-4-1-${i}" data-localize="question-type" required>
+      <input type="number" placeholder="Question price(optional)" id="question-cost-4-1-${i}" data-localize="question-price">      
       <h5 data-localize="optional">Optional</h5>
       <div id="audio_cont-4-1-${i}">
         <label for="audio" data-localize="input-audio">Select audio for a question</label>
@@ -41,8 +43,8 @@ const view = () => {
       </div>`).join('\n');
   }
 
-  const themesList = roundNumber => {
-    return [1, 2, 3, 4, 5].map(i => `
+  const themesList = (roundNumber, themeCount = 5, questionCount = 5) => {
+    return Array(themeCount).fill(1).map((x, i) => i + 1).map(i => `
         <h3><span data-localize="category">Category</span> ${i}</h3>
         <div class="collapse show">
           <h4 data-localize="category-name">Category name</h4>
@@ -51,25 +53,25 @@ const view = () => {
           <h5 data-localize="secret-question">Secret question</h5>
           <select id="secretIndex-select-${roundNumber}-${i}" class="form-control">
             <option>none</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+            ${Array(questionCount).fill(1).map((x, i) => i + 1).map(i => `<option>${i}</option>`).join('\n')}
           </select>
           <h5 data-localize="bet-question">Bet question</h5>
           <select id="betIndex-select-${roundNumber}-${i}" class="form-control">
             <option>none</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+            ${Array(questionCount).fill(1).map((x, i) => i + 1).map(i => `<option>${i}</option>`).join('\n')}
           </select>
-          ${questionList(roundNumber, i)}
+          ${questionList(roundNumber, i, questionCount)}
         </div>
         <br>
     `).join('\n');
+  }
+
+  const roundList = (roundCount = 3, themeCount = 5, questionCount = 5) => {
+    return Array(roundCount).fill(1).map((x, i) => i + 1)
+      .map(i => `<h2 class="collapse-control" id="ref_round${i}"><span class="collapse-control" id="ref2_round${i}" data-localize="round">Round</span> ${i} ➔</h2>
+      <div class="collapse" id="round${i}">
+        ${themesList(i, themeCount, questionCount)}
+      </div>`).join('\n');
   }
 
   return `<div class="container">
@@ -92,6 +94,8 @@ const view = () => {
         </label>
       </div>
       <br>
+      
+      <!--
       <h2 class="collapse-control" id="ref_round1"><span class="collapse-control" id="ref2_round1" data-localize="round">Round</span> 1 ➔</h2>
       <div class="collapse" id="round1">
         ${themesList(1)}
@@ -104,6 +108,10 @@ const view = () => {
       <div class="collapse" id="round3">
         ${themesList(3)}
       </div>
+      -->
+      
+      ${roundList(3, 5, 5)}
+      
       <h2 class="collapse-control" id="ref_round-fin"><span data-localize="final-round" class="collapse-control" id="ref2_round-fin">Final Round</span> ➔</h2>
       <div class="collapse" id="round-fin">
         ${finalQuestionList()}
